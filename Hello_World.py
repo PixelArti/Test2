0ad97a7c7c458329f42ac842bb391b9255895548
PixelArti <artiom.r.m@gmail.com>
2023-06-03 21:21:11 +0300

    firstcommit

---
+++ Hello_World.py
@@ -0,0 +1,18 @@
+
+def arti():
+    aa = 1
+    
+class Artiman:
+    def __init__(self) -> None:
+        super().__init__()
+        a = 1
+        
+        
+b, c, d = 1
+e = 2
+man = [b, c, d]
+
+if c == b:
+    print("a")
+    
+    
